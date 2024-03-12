4f980f062bc066f663864af212a12570d15f7948
markj <sc.markj0307@gmail.com>
2024-03-13 00:43:34 +0800

    initialize spring

---
+++ server/src/main/java/com/markj/onlinerecipes/OnlineRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.markj.onlinerecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineRecipesApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/markj/onlinerecipes/OnlineRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markj.onlinerecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
